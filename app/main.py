23cd08431daf0776fdc3f848b4eb4fd506553564
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-16 18:51:02 +0500

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -13,11 +13,25 @@ def decode_bencode(bencoded_value):
             raise ValueError("Invalid encoded value")
         return int(bencoded_value[1:end_index])
     elif chr(bencoded_value[0]) == 'l':
-        end_index = bencoded_value.find(b'e')
-        if end_index == -1:
-            raise ValueError("Invalid encoded value")
-        list_items = bencoded_value[1:end_index].split(b'e')
-        return [decode_bencode(item + b'e') for item in list_items]
+        list_items = []
+        start_index = 1
+        while start_index < len(bencoded_value) - 1:
+            if chr(bencoded_value[start_index]).isdigit():
+                end_index = bencoded_value.find(b':', start_index)
+                length = int(bencoded_value[start_index:end_index])
+                start_index = end_index + 1 + length
+                list_items.append(bencoded_value[end_index+1:start_index])
+            elif chr(bencoded_value[start_index]) == 'i':
+                end_index = bencoded_value.find(b'e', start_index)
+                list_items.append(int(bencoded_value[start_index+1:end_index]))
+                start_index = end_index + 1
+            elif chr(bencoded_value[start_index]) == 'l':
+                end_index = start_index
+                while bencoded_value.count(b'e', start_index, end_index+1) != bencoded_value.count(b'l', start_index, end_index+1):
+                    end_index += 1
+                list_items.append(decode_bencode(bencoded_value[start_index:end_index+1]))
+                start_index = end_index + 1
+        return list_items
     else:
         raise NotImplementedError("Only strings, integers and lists are supported at the moment")
 
