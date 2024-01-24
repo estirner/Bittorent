5b5dfa62b9e841c5908d6d4c152ba518afe2fb85
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-25 00:05:42 +0500

    pass tenth stage

--- app/main.py
+++ app/main.py
@@ -180,8 +180,26 @@ def main():
         with open(sys.argv[2], 'rb') as f:
             bencoded_value = f.read()
         torrent_info, _ = decode_bencode(bencoded_value)
-        peers = get_peers(torrent_info)
-        print(*peers, sep="\n")
+        tracker_url = torrent_info.get('announce', '').decode()
+        info_dict = torrent_info.get('info', {})
+        bencoded_info = bencode(info_dict)
+        info_hash = hashlib.sha1(bencoded_info).digest()
+        params = {
+            'info_hash': info_hash,
+            'peer_id': '00112233445566778899',
+            'port': 6881,
+            'uploaded': 0,
+            'downloaded': 0,
+            'left': torrent_info.get('info', {}).get('length', 0),
+            'compact': 1
+        }
+        response = requests.get(tracker_url, params=params)
+        response_dict, _ = decode_bencode(response.content)
+        peers = response_dict.get('peers', b'')
+        for i in range(0, len(peers), 6):
+            ip = '.'.join(str(b) for b in peers[i:i+4])
+            port = struct.unpack('!H', peers[i+4:i+6])[0]
+            print(f"Peer: {ip}:{port}")
     elif command == "handshake":
         with open(sys.argv[2], 'rb') as f:
             bencoded_value = f.read()
